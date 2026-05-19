a004f6a4a4b858886adaaf63fc61660370242801
enricoding <enricoding.dev@gmail.com>
2026-05-19 22:08:56 +0200

    Update and rename main.py to 001.py

---
+++ day-001/001.py
@@ -0,0 +1,29 @@
+#Printing
+# Write your code below this line 👇
+print("Hello world!")
+
+#String Manipulation
+# Write your code below this line 👇
+print("Hello world!\nHello world!\nHello world!")
+print("Hello" + " " + "Enrico")
+
+#Inputs
+#This line of code will take an input using input() function
+print("Hello " + input("What is your name?") + "!")
+
+#Variabels
+#print(len(input("What is your name?")))
+username = input("What is your name?")
+length = len(username)
+print(length)
+
+#Variables Naming
+name = "Angela"
+length = len(name)
+print(length)
+
+#Band Name Generator Project
+print("Welcome to the Band Name Generator.")
+city = input("Which city did you grow up in?\n")
+pet = input("What is the name of a pet?\n")
+print("Your band name could be " + city + " " + pet)
